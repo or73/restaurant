42ef2f32ef316d3d56d393c96dcc269a700c7135
OR73 <oreyesc@gmail.com>
2019-04-18 21:08:36 -0500

    'redirect' function is added and 'newmenuitem.html' has been corrected The 'redirect' function has been used to redirect the URL to a new desired URL, when is required.  And, a 'flash' function is added to show a message alarm when a function has been completed successfully, to validate its functionality and shows an informative message to the customer.

--- restaurant.py
+++ restaurant.py
@@ -1,7 +1,7 @@
 """
 Flask application with DB & Links
 """
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -28,13 +28,11 @@ def restaurantMenu(restaurant_id):
            methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
     if request.method == 'POST':
-        print('newMenuItem - POST')
         newItem = MenuItem(
             name=request.form['name'], restaurant_id=restaurant_id)
-        print('newItem: ', newItem)
         session.add(newItem)
         session.commit()
-        print('newItem added')
+        flash('New Menu Item has been created!!!')
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
         return render_template('newmenuitem.html', restaurant_id=restaurant_id)
@@ -50,6 +48,7 @@ def editMenuItem(restaurant_id, menu_id):
             editedItem.name = request.form['name']
         session.add(editedItem)
         session.commit()
+        flash('Menu Item has been edited!!!')
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
         return render_template(
@@ -64,11 +63,13 @@ def deleteMenuItem(restaurant_id, menu_id):
     if request.method == 'POST':
         session.delete(itemToDelete)
         session.commit()
+        flash('Menu Item has been deleted!!!')
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
         return render_template('deletemenuitem.html', item=itemToDelete)
 
 
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
