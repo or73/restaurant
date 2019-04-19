3767ad79fe50c9e2481f21fe499a387a42cf4861
OR73 <oreyesc@gmail.com>
2019-04-18 20:57:13 -0500

    A 'newmenuitem.html' template has been added, and the newMenuItem function is complete The 'newmenuitem.html' template allows to add a new item to specified menu by filling a form, through the path: '/restaurants/<int:restaurant_id>/new/' with a POST method.

--- restaurant.py
+++ restaurant.py
@@ -1,12 +1,12 @@
 """
 Flask application with DB & Links
 """
-from flask import Flask, render_template
-app = Flask(__name__)
-
+from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
+
+app = Flask(__name__)
 
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
@@ -19,19 +19,24 @@ session = DBSession()
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-    items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id)
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
     return render_template('menu.html', restaurant=restaurant, items=items)
 
 
-@app.route('/restaurants/<int:restaurant_id>/edit/')
-def restaurantEdit(restaurant_id):
-    message = ''
-
-
 # Task1: Create route for newMenuItem
-@app.route('/restaurants/<int:restaurant_id>/new/')
+@app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
-    return 'Page to create a new menu item.  Task 1 completed'
+    if request.method == 'POST':
+        print('newMenuItem - POST')
+        newItem = MenuItem(
+            name=request.form['name'], restaurant_id=restaurant_id)
+        print('newItem: ', newItem)
+        session.add(newItem)
+        session.commit()
+        print('newItem added')
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template('newmenuitem.html', restaurant_id=restaurant_id)
 
 
 # Task2: Create route for editMenuItem
