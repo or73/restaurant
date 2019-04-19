f79d298ae87afc514e4907ba36ffad40d81a3906
OR73 <oreyesc@gmail.com>
2019-04-18 20:27:32 -0500

    This is the updated Python-Flask app, with DB connection functionality. The app connects to a local database, called restaurant.db, and contains all information about the restaurant menus and items of each menu.  In this step a DB connection is tested.

--- restaurant.py
+++ restaurant.py
@@ -1,5 +1,5 @@
 """
-Minimum Flask application
+Flask application with DB
 """
 
 
@@ -7,13 +7,27 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 
 @app.route('/')
 @app.route('/hello')
-
-
 def HelloWorld():
-    return 'Hello World'
+    restaurant = session.query(Restaurant).first()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    message = ''
+    for i in items:
+        message += i.name
+        message += '<br/>'
+    return message
 
 
 if __name__ == '__main__':
